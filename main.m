08b92d359f9061af1192941ea5bab6c6055a1056
tommasomac <46065096+tommasomac@users.noreply.github.com>
2018-12-28 21:37:47 +0100

    updated with the full trajectory

--- main.m
+++ main.m
@@ -101,6 +101,121 @@ end
 
 % initialize plot
 num_landmarks_tracked = [zeros(1,19), length(init_landmarks)];
+T_W_C_all_vec = zeros(3,1);
+T_W_C_all_hom_old = eye(4);
+T_W_C_hom_old = eye(4);
+
+%% Continuous operation
+
+% fprintf('\n Press any key to start the continous operation...');
+% pause; %% TODO remove before hand-in
+
+% Setup
+range = (bootstrap_frames(2)+1):last_frame;
+prev_img = img1;
+prev_state = initializeState(init_keypoints, init_landmarks);
+
+% Process frames
+for i = range
+    fprintf('\n\nProcessing frame %d\n=====================\n', i);
+    if ds == 0
+        image = imread([kitti_path '/00/image_0/' sprintf('%06d.png',i)]);
+
+    elseif ds == 1
+        image = rgb2gray(imread([malaga_path ...
+            '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
+            left_images(i).name]));
+    elseif ds == 2
+        image = im2uint8(rgb2gray(imread([parking_path ...
+            sprintf('/images/img_%05d.png',i)])));
+
+    elseif ds == 3
+        image = im2uint8(imread([ascento_path ...
+            sprintf('/images/img_%05d.png',i)]));
+
+    else
+        assert(false);
+
+    end
+
+    % Update state and camera pose
+    [curr_state, T_W_C_curr] = processFrame(image, prev_img, prev_state, params);
+
+    % Make sure that plots refresh
+    pause(0.01);
+    
+    % PLOT
+    num_landmarks_tracked = [num_landmarks_tracked(1,2:end), length(curr_state.X)];
+    
+    T_W_C_curr_hom_new = T_W_C_hom_old * [T_W_C_curr ; 0 0 0 1];
+    
+    T_W_C_vec = T_W_C_curr_hom_new(1:3,4);
+    
+    
+    T_W_C_all_vec=[T_W_C_all_vec, T_W_C_vec];
+    
+    mainPlot(curr_state, T_W_C_curr, image, num_landmarks_tracked,T_W_C_all_vec,ground_truth);
+    
+    %END PLOT
+    
+    prev_img = image;
+    prev_state = curr_state;
+end
+
+elseif ds == 3
+    % Path containing images, depths and all...
+    assert(exist('ascento_path', 'var') ~= 0);
+    last_frame = 200;
+    K = load([ascento_path '/K.txt']);
+    load([ascento_path '/est_states.mat']);
+
+else
+    assert(false);
+
+end
+
+% set camera parameters (! matlab uses transposed K matrix)
+params.cam = cameraParameters('IntrinsicMatrix', K.');
+
+%% Bootstrap
+% frames used for initial bootstrapping
+bootstrap_frames = params.bootstrap_frames;
+
+if ds == 0
+    img0 = imread([kitti_path '/00/image_0/' ...
+        sprintf('%06d.png',bootstrap_frames(1))]);
+    img1 = imread([kitti_path '/00/image_0/' ...
+        sprintf('%06d.png',bootstrap_frames(2))]);
+
+elseif ds == 1
+    img0 = rgb2gray(imread([malaga_path ...
+        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
+        left_images(bootstrap_frames(1)).name]));
+    img1 = rgb2gray(imread([malaga_path ...
+        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
+        left_images(bootstrap_frames(2)).name]));
+
+elseif ds == 2
+    img0 = rgb2gray(imread([parking_path ...
+        sprintf('/images/img_%05d.png',bootstrap_frames(1))]));
+    img1 = rgb2gray(imread([parking_path ...
+        sprintf('/images/img_%05d.png',bootstrap_frames(2))]));
+
+elseif ds == 3
+    img0 = imread([ascento_path ...
+        sprintf('/images/img_%05d.png',bootstrap_frames(1))]);
+    img1 = imread([ascento_path ...
+        sprintf('/images/img_%05d.png',bootstrap_frames(2))]);
+
+else
+    assert(false);
+
+end
+
+[init_pose, init_keypoints, init_landmarks] = initialize(img0, img1, params);
+
+% initialize plot
+num_landmarks_tracked = [zeros(1,19), length(init_landmarks)];
 T_W_C_all=zeros(3,1);
 
 %% Continuous operation
