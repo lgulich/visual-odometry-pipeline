f1e56c3c0342f3166201a75232192ae792ce037c
vklemm <vklemm@student.ethz.ch>
2019-01-04 18:02:16 +0100

    correction of tf

--- lib/interesting_feature/tf2RobotPose.m
+++ lib/interesting_feature/tf2RobotPose.m
@@ -9,9 +9,24 @@ function robot_pose_vo = tf2RobotPose(T_W_C, theta)
 % Estimates the current robot pose from the current visual odometry camera
 % transformation matrix
 
-robot_pose_vo(1) = T_W_C(1,4);
-robot_pose_vo(2) = T_W_C(3,4);
-euler_pose = rotm2eul(T_W_C(1:3,1:3));
+%% correct tf matrix
+% compute current tf matrix to ground
+con_adj_t = [0 -1 0; 0 0 -1; 1 0 0];
+trans_init = [133.1203e-3; -0.2500e-3; 381.9169e-3];
+rot_init = [0.9998         0   -0.0193;...
+                         0    1.0000         0;...
+                         0.0193         0    0.9998];
+tans_corr = rotx(-rad2deg(theta))*con_adj_t*trans_init;
+            rot_corr = rotx(rad2deg(theta))*rot_init;
+tf_curr = [rot_corr, tans_corr; 0 0 0 1];
+
+% correct camera tf matrix
+T_W_C_corr = tf_curr\T_W_C;
+            
+%% assemble pose by projection
+robot_pose_vo(1) = T_W_C_corr(1,4);
+robot_pose_vo(2) = T_W_C_corr(3,4);
+euler_pose = rotm2eul(T_W_C_corr(1:3,1:3));
 robot_pose_vo(3) = euler_pose(2);
 robot_pose_vo(4) = theta;
 
