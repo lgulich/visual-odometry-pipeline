a21bcdba88cca0fa1c021dc4dd18f2f7e7e2f47c
vklemm <vklemm@student.ethz.ch>
2019-01-04 17:52:38 +0100

    vo tf implemented

--- lib/interesting_feature/tf2RobotPose.m
+++ lib/interesting_feature/tf2RobotPose.m
@@ -9,12 +9,10 @@ function robot_pose_vo = tf2RobotPose(T_W_C, theta)
 % Estimates the current robot pose from the current visual odometry camera
 % transformation matrix
 
-
-
-
-robot_pose_vo(1) = 0;
-robot_pose_vo(2) = 0;
-robot_pose_vo(3) = 0;
+robot_pose_vo(1) = T_W_C(1,4);
+robot_pose_vo(2) = T_W_C(3,4);
+euler_pose = rotm2eul(T_W_C(1:3,1:3));
+robot_pose_vo(3) = euler_pose(2);
 robot_pose_vo(4) = theta;
 
 end
--- main.m
+++ main.m
@@ -63,6 +63,7 @@ elseif ds == 3
     load([ascento_path '/est_states.mat']);
     if af == 1
         est_states_start_index = 18;
+        simulate_vo = false;
     end
 else
     error('dataset not found');
@@ -160,19 +161,13 @@ for i = range
     end
     
     % Update state and camera pose
-%     [curr_state, T_W_C_curr] = processFrame(image, prev_img, prev_state, params);
-    %TODO VK
-    curr_state = prev_state;
-    T_W_C_curr = zeros(3,4);
+    [curr_state, T_W_C_curr] = processFrame(image, prev_img, prev_state, params);
     
     if af == 1
         if i > range(1)
             
             % update world iterators
             theta_curr = double(double(ascento_est_states{i*20-2}.EstThetaMean));
-            
-            % update vo state
-            robot_pose_vo_curr = tf2RobotPose(T_W_C_curr, theta_curr);
             
             % update wo state
             x_curr = double((-ascento_est_states{i*20-2}.EstRobotYPos) - x_initial);
@@ -183,18 +178,24 @@ for i = range
             robot_pose_wo_curr(2) = z_curr*cos(gamma_initial)-x_curr*sin(gamma_initial);
             robot_pose_wo_curr(3) = gamma_curr;
             robot_pose_wo_curr(4) = theta_curr;
-            
-            % get translation vector from vo
-            d_robot_pose_vo = robot_pose_vo_curr - robot_pose_vo_last;
+               
+            % update vo state
+            T_W_C_curr_rec = T_W_C_init_inv * [T_W_C_curr; 0 0 0 1];
+            robot_pose_vo_curr = tf2RobotPose(T_W_C_curr_rec, theta_curr);
             
             % get translation vector from wo
             d_robot_pose_wo = robot_pose_wo_curr - robot_pose_wo_last;
             
-            % Todo VK (simulates scale drift)
-            d_robot_pose_vo(1) = d_robot_pose_wo(1)*(1-i/1000);
-            d_robot_pose_vo(2) = d_robot_pose_wo(2)*(1-i/1000);
-            d_robot_pose_vo(3) = d_robot_pose_wo(3);
-            d_robot_pose_vo(4) = d_robot_pose_wo(4);
+            % get translation vector from vo
+            d_robot_pose_vo = robot_pose_vo_curr - robot_pose_vo_last;
+            
+            % simulate vo if desired
+            if simulate_vo == true
+                d_robot_pose_vo(1) = d_robot_pose_wo(1)*(1-i/1000); % for scale drift
+            	d_robot_pose_vo(2) = d_robot_pose_wo(2)*(1-i/1000); % for scale drift
+                d_robot_pose_vo(3) = d_robot_pose_wo(3);
+                d_robot_pose_vo(4) = d_robot_pose_wo(4);
+            end
             
             % combine translation vectors
             [d_robot_pose_W, kalman_state] = estimateDRobotPose(d_robot_pose_vo, d_robot_pose_wo, theta_curr, kalman_state);
@@ -202,15 +203,12 @@ for i = range
             % update W state
             robot_pose_W_curr = integrateRobotPose(robot_pose_W_last, d_robot_pose_W, theta_curr);
             
-            %TODO VK remove
-            robot_pose_vo_curr = integrateRobotPose(robot_pose_vo_last, d_robot_pose_vo, theta_curr);
-            
             % plot vo, wo and W robot poses
-            if(~mod(i,15))
+            if(~mod(i,15+range(2)) || i == range(2))
                 patch_bool = plotRobotPose(robot_pose_W_curr, 'r', image, -1.8, patch_bool);
-                patch_bool = plotRobotPose(robot_pose_vo_curr, 'b', image, 0, patch_bool);
                 patch_bool = plotRobotPose(robot_pose_wo_curr, 'c', image, 0, patch_bool);
-%                 plotCamera('Location',T_W_C_curr(1:3,4)-T_W_C_init(1:3,4),'Orientation',T_W_C_curr(1:3,1:3),'Size',0.05,'Color','k');
+                patch_bool = plotRobotPose(robot_pose_vo_curr, 'b', image, 0, patch_bool);
+                plotCamera('Location',T_W_C_curr_rec(1:3,4),'Orientation',T_W_C_curr_rec(1:3,1:3),'Size',0.05,'Color','k');
             end
             
             % update vo iterators
@@ -227,7 +225,7 @@ for i = range
             % initialize global quantities
             patch_bool = true; 
             kalman_state.X = [0.0; 0.0; 0.0; 1.0];
-            kalman_state.P = diag([0.0; 0.0; 0.0; 0.5].^2);
+            kalman_state.P = diag([0.0; 0.0; 0.0; 1.0].^2);
             
             % initialize W iterators
             theta_last = double(ascento_est_states{i*20-2}.EstThetaMean);
@@ -235,17 +233,8 @@ for i = range
             robot_pose_W_last(1)= 0.0;
             robot_pose_W_last(2)= 0.0;
             robot_pose_W_last(3)= 0.0;
-            robot_pose_W_last(4) = theta_last;
-            
-            % initialialize vo iterators
-            theta_last = double(ascento_est_states{i*20-2}.EstThetaMean);
-            T_W_C_init = T_W_C_curr;
-            
-            robot_pose_vo_last(1)= 0.0;
-            robot_pose_vo_last(2)= 0.0;
-            robot_pose_vo_last(3)= 0.0;
-            robot_pose_vo_last(4) = theta_last;
-            
+            robot_pose_W_last(4) = theta_last;       
+                        
             % initialize wo iterators
             robot_pose_wo_last(1)= 0.0;
             robot_pose_wo_last(2)= 0.0;
@@ -255,6 +244,22 @@ for i = range
             x_initial = double(-ascento_est_states{range(1)*20-2}.EstRobotYPos);
             z_initial = double(ascento_est_states{range(1)*20-2}.EstRobotXPos);
             gamma_initial = double(ascento_est_states{range(1)*20-2}.EstRobotGammaOrient);
+            
+            % initialialize vo iterators
+            con_adj_t = [0 -1 0; 0 0 -1; 1 0 0];
+            trans_init = [133.1203e-3; -0.2500e-3; 381.9169e-3];
+            rot_init = [0.9998         0   -0.0193;...
+                         0    1.0000         0;...
+                         0.0193         0    0.9998];
+            tans_corr = rotx(-rad2deg(theta_last))*con_adj_t*trans_init;
+            rot_corr = rotx(rad2deg(theta_last))*rot_init;
+            tf_init = [rot_corr, tans_corr; 0 0 0 1];
+            T_W_C_init_inv = tf_init/[T_W_C_curr; 0 0 0 1];
+            
+            robot_pose_vo_last(1)= 0.0;
+            robot_pose_vo_last(2)= 0.0;
+            robot_pose_vo_last(3)= 0.0;
+            robot_pose_vo_last(4) = theta_last;
             
         end
     end
