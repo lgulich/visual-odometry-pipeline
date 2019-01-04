b9513e4310fc78bee1ac1cd5aefea3a7260c5551
tommasomac <46065096+tommasomac@users.noreply.github.com>
2019-01-04 14:49:57 +0100

    deleted useless "record" script, added videoFromFrames script and improved a bit the plot. Also included Frame saver in the main

---
+++ lib/viz/videoFromFrames.m
@@ -0,0 +1,17 @@
+workingDir = 'plotFrames';
+imageNames = dir(fullfile('plotFrames','*.jpg'));
+imageNames = {imageNames.name}';
+
+%you can change the video file name here:
+outputVideo = VideoWriter(fullfile('videoPlot.avi'));
+
+%you can change the desired framerate here:
+outputVideo.FrameRate = 1;
+
+open(outputVideo)
+
+for ii = 1:length(imageNames)
+   img = imread(fullfile('plotFrames',imageNames{ii}));
+   writeVideo(outputVideo,img)
+end
+close(outputVideo)
